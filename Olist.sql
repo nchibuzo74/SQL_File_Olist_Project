09a76c88449723e40d567055eaf69446fb5aaa02
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-05 16:46:03 +0100

    Geolocation table was created

--- Olist.sql
+++ Olist.sql
@@ -1,7 +1,11 @@
----Retrieve the data
+---Retrieve the customer data
 select *
 from olist_datasets.customers;
 
 ---Unique Customers
 SELECT count(distinct(customer_id)) as unique_customers
 from olist_datasets.customers;
+
+----Retrieve the geolocation data
+select *
+from olist_datasets.geolocation;
