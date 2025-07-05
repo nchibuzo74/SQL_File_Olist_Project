4ac30b9e9ea1641f018774d24cf9b47fffba636a
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-05 18:30:27 +0100

    Distinct zip code was verified

--- Olist.sql
+++ Olist.sql
@@ -9,3 +9,7 @@ from olist_datasets.customers;
 ----Retrieve the geolocation data
 select *
 from olist_datasets.geolocation;
+
+---Distinct geolocation zipcode
+SELECT distinct geolocation_zip_code_prefix
+from olist_datasets.geolocation;
