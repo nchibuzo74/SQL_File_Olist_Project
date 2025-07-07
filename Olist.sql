ade8d50c1b97a5ee7bb2dc5deff907e5abce5acf
Chibuzo Nwankwo <nwankwochibuzosamuel24@gmail.com>
2025-07-07 09:53:56 +0100

    A unique dimension table for geolocation was created

--- Olist.sql
+++ Olist.sql
@@ -6,6 +6,12 @@ from olist_datasets.customers;
 SELECT count(distinct(customer_id)) as unique_customers
 from olist_datasets.customers;
 
+---Check for duplicate customers
+SELECT customer_id, count(customer_id) as customer_count
+from olist_datasets.customers
+group by customer_id
+having count(customer_id) > 1;
+
 ----Retrieve the geolocation data
 select *
 from olist_datasets.geolocation;
@@ -13,3 +19,13 @@ from olist_datasets.geolocation;
 ---Distinct geolocation zipcode
 SELECT distinct geolocation_zip_code_prefix
 from olist_datasets.geolocation;
+
+---Create a unique table with geolocation data
+SELECT geolocation_zip_code_prefix,
+geolocation_state as state,
+geolocation_city as city,
+max(geolocation_lat) as max_lat,
+max(geolocation_lng) as max_lng
+from olist_datasets.geolocation
+group by geolocation_zip_code_prefix, 
+geolocation_state, geolocation_city;
